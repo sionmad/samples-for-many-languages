b0b1a7e4822072f6fae40c6d353bdefa13eb2e1e
sionmad <180800158+sionmad@users.noreply.github.com>
2026-08-05 16:09:18 +0900

    added C#

---
+++ C#/hello.cs
@@ -0,0 +1,15 @@
+/*
+ * C# Hello World Sample
+ * Author: sionmad
+ */
+
+using System;
+
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to C#!");
+    }
+}
