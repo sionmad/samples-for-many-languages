ed7b9e0911ffc291b6415e954c37c31f9e3f3c0c
sionmad <180800158+sionmad@users.noreply.github.com>
2026-08-05 16:03:25 +0900

    added java

---
+++ java/hello.java
@@ -0,0 +1,11 @@
+/**
+ * Java Hello World Sample
+ * Author: sionmad
+ */
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java!");
+    }
+}
