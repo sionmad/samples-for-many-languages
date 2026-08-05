3562a8952145a8de8fc82aeb8eb0801fff6c1cce
sionmad <180800158+sionmad@users.noreply.github.com>
2026-08-05 16:02:46 +0900

    added typescript

---
+++ typescript/hello.ts
@@ -0,0 +1,11 @@
+/**
+ * TypeScript Hello World Sample
+ * Author: sionmad
+ */
+
+function main(): void {
+    console.log("Hello, World!");
+    console.log("Welcome to TypeScript!");
+}
+
+main();
